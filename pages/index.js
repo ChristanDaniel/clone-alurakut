a24906bd70ae1e3c70a955252bb51d3ffc780da8
ChristianDaniel <christian_daniel04@hotmail.com>
2021-07-16 13:27:45 -0300

    add componente profileRelations

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Box from '../src/components/Box'
 import MainGrid from '../src/components/MainGrid'
-import { AlurakutMenu } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/profileRelations'
 
 // const Title = styled.h1`
 //   font-size: 50px;
@@ -29,15 +30,42 @@ export default function Home() {
         </div>
 
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
-          <Box> Bem Vindo </Box>
+
+          <Box>
+            <h1 className="title">
+            Bem Vindo(a)
+            </h1>
+
+           <OrkutNostalgicIconSet />
+          </Box>
+
         </div>
       
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
-          <Box>Pessoas da comunidades </Box>
-          <Box> Comunidades </Box>
+
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidades ({pessoasFavoritas.length})
+            </h2>
+
+            <ul>
+                {pessoasFavoritas.map((itemAtual) => {
+                  return(
+                    <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`http://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                   </li>
+                  )
+                })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
         </div>
 
       </MainGrid>
    </>
   )
 }
+
+{/* <Box> Comunidades </Box> */}
