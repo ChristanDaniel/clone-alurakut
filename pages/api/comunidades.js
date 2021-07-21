372c152947012c3a1b95f91c8452efe025fbac10
ChristianDaniel <christian_daniel04@hotmail.com>
2021-07-20 23:00:09 -0300

    teste

--- pages/api/comunidades.js
+++ pages/api/comunidades.js
@@ -1,18 +1,18 @@
-import { SiteClient } from 'datocms-client'
+import { SiteClient } from 'datocms-client';
 
 
-export default async function recebedorDeRequest(resquest, response) {
+export default async function recebedorDeRequests(resquest, response) {
     const TOKEN = '5a76ebb66ef94135cc8b0c27bc0ec6'
     const client = new SiteClient(TOKEN)
 
-    const registroCriado = client.items.create({
+    const registroCriado = await client.items.create({
         itemType: "980120",
-        title: "",
-        id: "",
-        imageUrl: "",
-        creatorSlug: "",
+        title: "teste",
+        imageUrl: "http://github.com/ChristanDaniel.png",
+        creatorSlug: "ChristanDaniel",
 
     })
+    console.log(registroCriado)
 
     response.json({
         dados: 'teste',
