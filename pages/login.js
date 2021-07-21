00df8e559a4b0cc21ccd763740f103e876de5554
ChristianDaniel <christian_daniel04@hotmail.com>
2021-07-21 14:49:41 -0300

    front end do login finalizado

--- pages/login.js
+++ pages/login.js
@@ -12,7 +12,44 @@ export default function LoginScreen() {
           <p><strong>Compartilhe</strong> seus vídeos, fotos e paixões em um só lugar</p>
         </section>
 
-        
+        <section className="formArea">
+          <form className="box">
+            <p>
+              Acesse agora mesmo com seu usuário do <strong>GitHub</strong>!
+          </p>
+            <input
+                placeholder="Usuário"
+                value={githubUser}
+                onChange={(evento) => {
+                    setGithubUser(evento.target.value)
+                }}
+            />
+            {githubUser.length === 0
+                ? 'Preencha o campo'
+                : ''
+            }
+            <button type="submit">
+              Login
+            </button>
+          </form>
+
+          <footer className="box">
+            <p>
+              Ainda não é membro? <br />
+              <a href="/login">
+                <strong>
+                  ENTRAR JÁ
+              </strong>
+              </a>
+            </p>
+          </footer>
+        </section>
+
+        <footer className="footerArea">
+          <p>
+            © 2021 alura.com.br - <a href="/">Sobre o Orkut.br</a> - <a href="/">Centro de segurança</a> - <a href="/">Privacidade</a> - <a href="/">Termos</a> - <a href="/">Contato</a>
+          </p>
+        </footer>
       </div>
     </main>
   )
