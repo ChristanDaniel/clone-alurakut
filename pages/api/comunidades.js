7538ee7f31e01d8ef7da068b3e9755b5b162355d
ChristianDaniel <christian_daniel04@hotmail.com>
2021-07-20 22:48:29 -0300

    finalizando api com datocms

--- pages/api/comunidades.js
+++ pages/api/comunidades.js
@@ -1,7 +1,21 @@
+import { SiteClient } from 'datocms-client'
+
+
 export default async function recebedorDeRequest(resquest, response) {
     const TOKEN = '5a76ebb66ef94135cc8b0c27bc0ec6'
-    console.log(TOKEN)
+    const client = new SiteClient(TOKEN)
+
+    const registroCriado = client.items.create({
+        itemType: "980120",
+        title: "",
+        id: "",
+        imageUrl: "",
+        creatorSlug: "",
+
+    })
+
     response.json({
-        dados: 'teste'
+        dados: 'teste',
+        registroCriado: registroCriado,
     })
 }
