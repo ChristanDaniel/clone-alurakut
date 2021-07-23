7e906bbcc2231dc41206dd35047de2e5b0b4a49c
ChristianDaniel <christian_daniel04@hotmail.com>
2021-07-23 14:25:13 -0300

    logout criado

--- pages/Logout.js
+++ pages/Logout.js
@@ -4,5 +4,10 @@ export default function Logout() {}
 
 export async function getServerSideProps(context) {
   nookies.destroy(context, 'USER_TOKEN')
-  
+  return {
+    redirect: {
+      destination: '/login',
+      permanent: false,
+    }
+  }
 }
